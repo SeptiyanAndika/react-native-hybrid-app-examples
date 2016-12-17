cb66645516868642d40443084543380c4b90bb96
Jeremy Gale <jeremy@forcegrind.com>
2016-12-16 21:45:44 -0700

    Use the modern way of loading the React Native bundle in appDelegate. One of the old ways works, the other doesn't

--- iOS/RNEmbeddedAppExample/AppDelegate.m
+++ iOS/RNEmbeddedAppExample/AppDelegate.m
@@ -9,6 +9,7 @@
 
 #import "AppDelegate.h"
 #import "MenuViewController.h"
+#import "RCTBundleURLProvider.h"
 
 @implementation AppDelegate
 
@@ -18,34 +19,8 @@
 {
     NSURL *jsCodeLocation;
     
-    /**
-     * Loading JavaScript code - uncomment the one you want.
-     *
-     * OPTION 1
-     * Load from development server. Start the server from the repository root:
-     *
-     * $ npm start
-     *
-     * To run on device, change `localhost` to the IP address of your computer
-     * (you can get this by typing `ifconfig` into the terminal and selecting the
-     * `inet` value under `en0:`) and make sure your computer and iOS device are
-     * on the same Wi-Fi network.
-     */
-    
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
-    
-    /**
-     * OPTION 2
-     * Load from pre-bundled file on disk. To re-generate the static bundle
-     * from the root of your project directory, run
-     *
-     * $ react-native bundle --minify
-     *
-     * see http://facebook.github.io/react-native/docs/runningondevice.html
-     */
-    
-    //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-    
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+
     // Here we are instantiating the `RCTBridge` to be used in other parts of the app later.
     bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation
                                    moduleProvider:nil
